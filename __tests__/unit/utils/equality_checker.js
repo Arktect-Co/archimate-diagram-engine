fc9063be6a2f1d2b9b28d52f1c64f2ae285eeed1
Diorbert Pereira <diorbert@gmail.com>
2022-04-07 20:18:03 -0300

    Initial setup for unit testing

---
+++ __tests__/unit/utils/equality_checker.js
@@ -0,0 +1,11 @@
+/**
+ * Verifies if a graph represents a view correctly
+ * @param viewInput View object describing view nodes and relationships
+ * @param graphOutput JointJS graph exported to JSON
+ * @return true if the graph is equal to the view
+ */
+function checkModelsEquality (viewInput, graphOutput){
+    return true;
+}
+
+module.exports = checkModelsEquality;
