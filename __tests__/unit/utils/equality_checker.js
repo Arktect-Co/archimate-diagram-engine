3573e3f293e1170ffe1f8593985929814f83e44e
Diorbert Pereira <diorbert@gmail.com>
2022-04-07 20:52:05 -0300

    Equality checking returns false when an error occurs

--- __tests__/unit/utils/equality_checker.js
+++ __tests__/unit/utils/equality_checker.js
@@ -5,41 +5,46 @@
  * @return true if the graph is equal to the view
  */
 function checkModelsEquality(viewInput, graphOutput) {
-    if (Array.isArray(viewInput.viewNodes) && Array.isArray(viewInput.viewRelationships) &&
-        Array.isArray(graphOutput.cells)) {
-        for (let i = 0; i < viewInput.viewNodes.length; i++) {
-            const node = viewInput.viewNodes[i];
-            const cell = graphOutput.cells.find(e => e.id === node.viewNodeId);
+    try {
+        if (Array.isArray(viewInput.viewNodes) && Array.isArray(viewInput.viewRelationships) &&
+            Array.isArray(graphOutput.cells)) {
+            for (let i = 0; i < viewInput.viewNodes.length; i++) {
+                const node = viewInput.viewNodes[i];
+                const cell = graphOutput.cells.find(e => e.id === node.viewNodeId);
 
-            // If not found, then is different
-            if (!cell) {
-                return false;
+                // If not found, then is different
+                if (!cell) {
+                    return false;
+                }
+
+                // Checking attributes equality
+                if (cell.attrs.label.textWrap.text !== node.name || cell.modelElementType !== node.type ||
+                    cell.size.width !== node.width || cell.size.height !== node.height) {
+                    return false;
+                }
             }
 
-            // Checking attributes equality
-            if (cell.attrs.label.textWrap.text !== node.name || cell.modelElementType !== node.type ||
-                cell.size.width !== node.width || cell.size.height !== node.height) {
-                return false;
+            for (let i = 0; i < viewInput.viewRelationships.length; i++) {
+                const rel = viewInput.viewRelationships[i];
+                const cell = graphOutput.cells.find(e => e.id === rel.viewRelationshipId);
+
+                // If not found, then is different
+                if (!cell) {
+                    return false;
+                }
+
+                // Checking attributes equality
+                if (cell.source.id !== rel.sourceId || cell.target.id !== rel.targetId ||
+                    cell.relationshipType !== rel.type) {
+                    return false;
+                }
             }
         }
 
-        for (let i = 0; i < viewInput.viewRelationships.length; i++) {
-            const rel = viewInput.viewRelationships[i];
-            const cell = graphOutput.cells.find(e => e.id === rel.viewRelationshipId);
-
-            // If not found, then is different
-            if (!cell) {
-                return false;
-            }
-
-            // Checking attributes equality
-            if (cell.source.id !== rel.sourceId || cell.target.id !== rel.targetId || cell.relationshipType !== rel.type) {
-                return false;
-            }
-        }
+        return true;
+    } catch (e) {
+        return false;
     }
-
-    return true;
 }
 
 module.exports = checkModelsEquality;
