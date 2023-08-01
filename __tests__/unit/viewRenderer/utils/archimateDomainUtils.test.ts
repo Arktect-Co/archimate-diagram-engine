a14a0036cc9a008aaa8a37329916c27972079ac0
Rodrigo Limões <rolimoes@gmail.com>
2023-08-01 17:19:21 -0300

    test: Ensure return the "motivational" value if the type is related to classification

--- __tests__/unit/viewRenderer/utils/archimateDomainUtils.test.ts
+++ __tests__/unit/viewRenderer/utils/archimateDomainUtils.test.ts
@@ -1,5 +1,7 @@
 import { typeToClassification } from '@lib/viewRenderer/utils/archimateDomainUtils';
 import { expect } from 'chai';
+import { NodeType } from '../../../../lib/common/enums/nodeType';
+import { NodeShapeClassification } from '../../../../lib/common/enums/nodeShapeClassification';
 
 describe('Archimate Domain Utils', () => {
   describe('typeToClassification', () => {
@@ -8,6 +10,13 @@ describe('Archimate Domain Utils', () => {
       const classification = typeToClassification(type);
 
       expect(classification).to.equal(type);
+    });
+
+    it('Should return the "motivational" value if the type is related to classification', () => {
+      const type = NodeType.Requirement;
+      const classification = typeToClassification(type);
+
+      expect(classification).to.equal(NodeShapeClassification.Motivational);
     });
   });
 });
