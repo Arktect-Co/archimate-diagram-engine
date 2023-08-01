b885647ae0339bf02bec271997f659e7da5c7461
Rodrigo Limões <rolimoes@gmail.com>
2023-08-01 18:27:45 -0300

    test: Ensure return the style setting of "layered"

--- __tests__/unit/viewRenderer/utils/styleUtility.test.ts
+++ __tests__/unit/viewRenderer/utils/styleUtility.test.ts
@@ -1,12 +1,19 @@
-import { getStyle } from '@lib/viewRenderer/utils/styleUtility';
+import { getStyle, Style } from '@lib/viewRenderer/utils/styleUtility';
 import themes from '@lib/viewRenderer/utils/style/styles';
 import { expect } from 'chai';
 
 describe('styleUtility', () => {
   it('should return the style setting of "element"', () => {
-    const style = 'element';
+    const style: Style = 'element';
     const theme = getStyle(style);
 
     expect(theme).to.equal(themes.COLOR_SCHEME_ELEMENT);
+  });
+
+  it('should return the style setting of "layered"', () => {
+    const style: Style = 'layered';
+    const theme = getStyle(style);
+
+    expect(theme).to.equal(themes.COLOR_SCHEME_LAYERED);
   });
 });
