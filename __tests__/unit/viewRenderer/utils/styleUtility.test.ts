8345e601ba4533734d97b1757a76a4af6a097a8a
Rodrigo Limões <rolimoes@gmail.com>
2023-08-01 18:33:56 -0300

    refactor: refactoring test case titles

--- __tests__/unit/viewRenderer/utils/styleUtility.test.ts
+++ __tests__/unit/viewRenderer/utils/styleUtility.test.ts
@@ -10,21 +10,21 @@ describe('styleUtility', () => {
     expect(theme).to.equal(themes.COLOR_SCHEME_HYBRID);
   });
 
-  it('should return the style setting of "element"', () => {
+  it('should return the "element" style configuration', () => {
     const style: Style = 'element';
     const theme = getStyle(style);
 
     expect(theme).to.equal(themes.COLOR_SCHEME_ELEMENT);
   });
 
-  it('should return the style setting of "layered"', () => {
+  it('should return the "layered" style configuration', () => {
     const style: Style = 'layered';
     const theme = getStyle(style);
 
     expect(theme).to.equal(themes.COLOR_SCHEME_LAYERED);
   });
 
-  it('should return the style setting of "hybrid"', () => {
+  it('should return the "hybrid" style configuration', () => {
     const style: Style = 'hybrid';
     const theme = getStyle(style);
 
