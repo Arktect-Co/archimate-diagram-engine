be92356719c12025bb86940d314d7d3a51e599d6
Rodrigo Limões <rolimoes@gmail.com>
2023-08-01 18:31:37 -0300

    test: Ensure return the "hybrid" style configuration if the informed style does not exist

--- __tests__/unit/viewRenderer/utils/styleUtility.test.ts
+++ __tests__/unit/viewRenderer/utils/styleUtility.test.ts
@@ -3,6 +3,13 @@ import themes from '@lib/viewRenderer/utils/style/styles';
 import { expect } from 'chai';
 
 describe('styleUtility', () => {
+  it('should return the "hybrid" style configuration if the informed style does not exist', () => {
+    const style = 'unknown' as Style;
+    const theme = getStyle(style);
+
+    expect(theme).to.equal(themes.COLOR_SCHEME_HYBRID);
+  });
+
   it('should return the style setting of "element"', () => {
     const style: Style = 'element';
     const theme = getStyle(style);
