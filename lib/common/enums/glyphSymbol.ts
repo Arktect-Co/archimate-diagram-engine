9ffada45de48cf569e8a28501ae33f38b8fd4938
Rodrigo Limões <rolimoes@gmail.com>
2023-08-01 16:38:15 -0300

    refactor: Adding glyphSymbol enum

---
+++ lib/common/enums/glyphSymbol.ts
@@ -0,0 +1,11 @@
+export enum GlyphSymbol {
+  Role = 'role',
+  Object = 'object',
+  Event = 'event',
+  Function = 'function',
+  Process = 'process',
+  DomainService = 'domainservice',
+  Interface = 'interface',
+  Collaboration = 'collaboration',
+  Interaction = 'interaction',
+}
