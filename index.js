03c0a2c99278a510f678b98fa5921bb8d52b5217
Rodrigo Limões <rolimoes@gmail.com>
2023-08-02 17:19:24 -0300

    refactor: Migrating the ViewSettings to Typescript

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const { ViewSettings } = require('@lib/viewRenderer/ViewSettings');
+
 module.exports = {
-    ViewRenderer: require("./lib/viewRenderer/ViewRenderer"),
-    ViewSettings: require("./lib/viewRenderer/ViewSettings")
+  ViewRenderer: require('@lib/viewRenderer/ViewRenderer'),
+  ViewSettings,
 };
