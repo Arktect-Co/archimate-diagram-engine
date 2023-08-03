8ff2fb2659fbfd6ae9ba93ec3045db2c61c06df8
Rodrigo Limões <rolimoes@gmail.com>
2023-08-03 15:49:01 -0300

    test: Ensure return a node with rectangular rounded shape if classification is "implementation_and_migration"

--- __tests__/unit/viewRenderer/nodeRendering/NodeBuilder.test.ts
+++ __tests__/unit/viewRenderer/nodeRendering/NodeBuilder.test.ts
@@ -48,5 +48,18 @@ describe('NodeBuilder', () => {
       expect(nodeShape.attributes.attrs.body).to.contain({ rx: 8, ry: 8 });
       expect(nodeShape.attributes.attrs.label).to.contain({ textAnchor: 'middle', refX: '42.5%' });
     });
+
+    it('should return a node with rectangular rounded shape if classification is "implementation_and_migration"', () => {
+      const nodeShape = nodeBuilder.buildShape({
+        type: NodeType.Gap,
+        name: 'unknown',
+        ...size,
+      });
+
+      expect(nodeShape.attributes.type).to.equal('standard.Rectangle');
+      expect(nodeShape.attributes.attrs.body).to.contain({ rx: 8, ry: 8 });
+      expect(nodeShape.attributes.size).to.contain(size);
+      expect(nodeShape.attributes.attrs.label).to.contain({ textAnchor: 'middle', refX: '42.5%' });
+    });
   });
 });
