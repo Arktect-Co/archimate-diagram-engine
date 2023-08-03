a9946ebab7d289978153e5ade5f4a5eb33042701
Rodrigo Limões <rolimoes@gmail.com>
2023-08-03 19:38:08 -0300

    test: Ensure return attributes with dashed stroke

--- __tests__/unit/viewRenderer/nodeRendering/ShapeBuilder.test.ts
+++ __tests__/unit/viewRenderer/nodeRendering/ShapeBuilder.test.ts
@@ -5,14 +5,15 @@ import { expect } from 'chai';
 describe('ShapeBuilder', () => {
   const shapeBuilder: ShapeBuilder = new ShapeBuilder(new ViewSettings({}));
 
+  const shapeAttributes = {
+    strokeColor: 'black',
+    fillColor: 'white',
+    elementName: 'Node 1',
+    refX: '42.5%',
+    textAnchor: 'middle',
+  };
+
   it('should return attributes', () => {
-    const shapeAttributes = {
-      strokeColor: 'black',
-      fillColor: 'white',
-      elementName: 'Node 1',
-      refX: '42.5%',
-      textAnchor: 'middle',
-    };
     const { body, label } = shapeBuilder.getAttributes(shapeAttributes);
 
     expect(label)
@@ -37,6 +38,41 @@ describe('ShapeBuilder', () => {
     expect(body).to.contain({
       fill: shapeAttributes.fillColor,
       stroke: shapeAttributes.strokeColor,
+      rx: 0,
+      ry: 0,
+      strokeWidth: 0.8,
+    });
+  });
+
+  it('should return attributes with dashed stroke', () => {
+    const { body, label } = shapeBuilder.getAttributes({
+      ...shapeAttributes,
+      withDashedStroke: true,
+    });
+
+    expect(label)
+      .to.deep.include({
+        textWrap: {
+          text: shapeAttributes.elementName,
+          width: '70%',
+          height: '100%',
+          ellipsis: true,
+        },
+      })
+      .contain({
+        fill: 'black',
+        textVerticalAnchor: 'top',
+        textAnchor: shapeAttributes.textAnchor,
+        refX: shapeAttributes.refX,
+        refY: 10,
+        refWidth: '100%',
+        refHeight: '100%',
+        fontSize: 12,
+      });
+    expect(body).to.contain({
+      fill: shapeAttributes.fillColor,
+      stroke: shapeAttributes.strokeColor,
+      strokeDasharray: '5,5',
       rx: 0,
       ry: 0,
       strokeWidth: 0.8,
