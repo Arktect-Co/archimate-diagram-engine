0673e6b91133a0f528306b9dcff146336bad0fb4
Rodrigo Limões <rolimoes@gmail.com>
2023-08-03 18:13:19 -0300

    test: Ensure return an error message if name does not exist

--- __tests__/unit/viewRenderer/nodeRendering/NodeBuilder.test.ts
+++ __tests__/unit/viewRenderer/nodeRendering/NodeBuilder.test.ts
@@ -151,18 +151,18 @@ describe('NodeBuilder', () => {
   });
 
   describe('buildNode', () => {
+    const attributes = {
+      width: 100,
+      height: 100,
+      type: NodeType.Node,
+      name: 'Node 1',
+      viewNodeId: '624f10ecd9f08d4030fe458f',
+      modelElementId: 'e2e60769-fea2-45e2-9355-66e092ec6934',
+      posX: 10,
+      posY: 10,
+      parentElement: undefined,
+    };
     it('should build a node in graph', () => {
-      const attributes = {
-        width: 100,
-        height: 100,
-        type: NodeType.Node,
-        name: 'Node 1',
-        viewNodeId: '624f10ecd9f08d4030fe458f',
-        modelElementId: 'e2e60769-fea2-45e2-9355-66e092ec6934',
-        posX: 10,
-        posY: 10,
-        parentElement: undefined,
-      };
       nodeBuilder.buildNode(attributes);
 
       const node = graph.getCell(attributes.viewNodeId).toJSON();
@@ -173,6 +173,17 @@ describe('NodeBuilder', () => {
       expect(node.size).to.contain({ width: attributes.width, height: attributes.height });
       expect(node.modelElementId).to.equal(attributes.modelElementId);
       expect(node.attrs.label.textWrap.text).to.equal(attributes.name);
+    });
+
+    it('should return an error message if name does not exist', () => {
+      try {
+        nodeBuilder.buildNode({ ...attributes, name: '' });
+      } catch (e) {
+        const { message } = e as Error;
+        expect(message).to.equal(
+          'Invalid node: Nodes must have viewNodeId, name and type defined.',
+        );
+      }
     });
   });
 });
