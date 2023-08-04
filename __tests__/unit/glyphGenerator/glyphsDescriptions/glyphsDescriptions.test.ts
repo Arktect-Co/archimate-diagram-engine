8a0bfc0026bd3bf8b2535d7d1a661d1363a28880
Rodrigo Limões <rolimoes@gmail.com>
2023-08-04 17:29:15 -0300

    test: Ensure return a "applicationcomponent" glyph

--- __tests__/unit/glyphGenerator/glyphsDescriptions/glyphsDescriptions.test.ts
+++ __tests__/unit/glyphGenerator/glyphsDescriptions/glyphsDescriptions.test.ts
@@ -1,10 +1,18 @@
 import { getGlyph } from '../../../../lib/glyphGenerator/glyphsDescriptions/glyphDescriptions';
 import { expect } from 'chai';
+import { NodeType } from '@lib/common/enums/nodeType';
+import { glyphs } from '@lib/glyphGenerator/glyphsDescriptions/glyphs';
 
 describe('glyphsDescriptions', () => {
   it('should return empty string if type does not contain glyph', () => {
     const glyph = getGlyph('unknow');
 
     expect(glyph).to.equal('');
+  });
+
+  it('should return a "applicationcomponent" glyph', () => {
+    const glyph = getGlyph(NodeType.ApplicationComponent);
+
+    expect(glyph).to.equal(glyphs[NodeType.ApplicationComponent]);
   });
 });
