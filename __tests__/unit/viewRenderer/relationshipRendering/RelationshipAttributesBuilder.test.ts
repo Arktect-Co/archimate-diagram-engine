ab607f524339debb95ce4ee00879c5118432f307
Rodrigo Limões <rolimoes@gmail.com>
2023-08-05 19:28:54 -0300

    test: Ensure return a serving relationship

--- __tests__/unit/viewRenderer/relationshipRendering/RelationshipAttributesBuilder.test.ts
+++ __tests__/unit/viewRenderer/relationshipRendering/RelationshipAttributesBuilder.test.ts
@@ -10,25 +10,41 @@ const builder = new EdgePointerBuilder(settings);
 describe('RelationshipAttributesBuilder', () => {
   const relationshipAttributesBuilder = new RelationshipAttributesBuilder(settings, builder);
 
-  it('should return a non bidirectional association relationship', () => {
-    const { line } = relationshipAttributesBuilder.buildAssociationRelationship(false);
+  describe('Association Relationship', () => {
+    it('should return a non bidirectional association relationship', () => {
+      const { line } = relationshipAttributesBuilder.buildAssociationRelationship(false);
 
-    expect(line.strokeWidth).to.equal(SETTINGS_DEFAULT.EDGE_WIDTH);
-    expect(line.stroke).to.equal(SETTINGS_DEFAULT.DARK_COLOR);
-    expect(line.targetMarker).to.contain({
-      type: 'path',
-      stroke: 'black',
-      d: 'M 12 -6 0 0 z',
+      expect(line.strokeWidth).to.equal(SETTINGS_DEFAULT.EDGE_WIDTH);
+      expect(line.stroke).to.equal(SETTINGS_DEFAULT.DARK_COLOR);
+      expect(line.targetMarker).to.contain({
+        type: 'path',
+        stroke: 'black',
+        d: 'M 12 -6 0 0 z',
+      });
+    });
+
+    it('should return a bidirectional association relationship', () => {
+      const { line } = relationshipAttributesBuilder.buildAssociationRelationship(true);
+
+      expect(line.strokeWidth).to.equal(SETTINGS_DEFAULT.EDGE_WIDTH);
+      expect(line.stroke).to.equal(SETTINGS_DEFAULT.DARK_COLOR);
+      expect(line.targetMarker).to.contain({
+        type: 'none',
+      });
     });
   });
 
-  it('should return a bidirectional association relationship', () => {
-    const { line } = relationshipAttributesBuilder.buildAssociationRelationship(true);
+  describe('ServingRelationship', () => {
+    it('should return a serving relationship', () => {
+      const { line } = relationshipAttributesBuilder.buildServingRelationship();
 
-    expect(line.strokeWidth).to.equal(SETTINGS_DEFAULT.EDGE_WIDTH);
-    expect(line.stroke).to.equal(SETTINGS_DEFAULT.DARK_COLOR);
-    expect(line.targetMarker).to.contain({
-      type: 'none',
+      expect(line.strokeWidth).to.equal(SETTINGS_DEFAULT.EDGE_WIDTH);
+      expect(line.stroke).to.equal(SETTINGS_DEFAULT.DARK_COLOR);
+      expect(line.targetMarker).to.contain({
+        type: 'path',
+        stroke: 'black',
+        d: 'M 12 -6 0 0 12 6 1 0 z',
+      });
     });
   });
 });
