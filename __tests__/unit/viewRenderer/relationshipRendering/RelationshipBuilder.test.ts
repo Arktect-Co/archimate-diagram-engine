983ab65e34aefbad10ceb6a94c7504d11c660b1b
Rodrigo Limões <rolimoes@gmail.com>
2023-08-06 15:43:42 -0300

    test: Ensure return default relationship attributes

--- __tests__/unit/viewRenderer/relationshipRendering/RelationshipBuilder.test.ts
+++ __tests__/unit/viewRenderer/relationshipRendering/RelationshipBuilder.test.ts
@@ -12,6 +12,20 @@ describe('RelationshipBuilder', () => {
   const builder = new RelationshipBuilder(graph, settings);
 
   describe('getRelationshipAttributes', () => {
+    it('should return default relationship attributes', () => {
+      const { line } = builder.getRelationshipAttributes({
+        type: 'unknown',
+        isBidirectional: false,
+      });
+
+      expect(line.strokeWidth).to.equal(SETTINGS_DEFAULT.EDGE_WIDTH);
+      expect(line.stroke).to.equal(SETTINGS_DEFAULT.DARK_COLOR);
+      expect(line.strokeDasharray).to.equal(3);
+      expect(line.targetMarker).to.contain({
+        type: PointerType.None,
+      });
+    });
+
     it('should return association relationship attributes', () => {
       const { line } = builder.getRelationshipAttributes({
         type: RelationshipType.Association,
