60b899fc2501b7608bb9f08db1d99160d7c617eb
Rodrigo Limões <rolimoes@gmail.com>
2023-08-23 15:13:30 -0300

    test: Ensure return a basic rectangular shape if node type is a "Representation"

--- __tests__/unit/viewRenderer/nodeRendering/NodeBuilder.test.ts
+++ __tests__/unit/viewRenderer/nodeRendering/NodeBuilder.test.ts
@@ -91,6 +91,18 @@ describe('NodeBuilder', () => {
       expect(nodeShape.attributes.size).to.contain(size);
     });
 
+    it('should return a basic rectangular shape if node type is a "Representation"', () => {
+      const nodeShape = nodeBuilderV3.buildShape({
+        type: NodeType.Representation,
+        name: 'Model Service',
+        ...size,
+      });
+
+      expect(nodeShape.attributes.type).to.equal('standard.Rectangle');
+      expect(nodeShape.attributes.attrs.body.fill).to.equal(colors.BUSINESS_PASSIVE);
+      expect(nodeShape.attributes.size).to.contain(size);
+    });
+
     it('should return a basic Octagonal shape if node type is a "Meaning"', () => {
       const nodeShape = nodeBuilderV3.buildShape({
         type: NodeType.Meaning,
