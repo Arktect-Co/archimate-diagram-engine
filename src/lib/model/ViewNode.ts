327ad810b2c6c14a6e00ba44242d3a53d21b4621
Rodrigo Limões <rolimoes@gmail.com>
2023-11-11 14:14:57 -0300

    fix: Adjusting type of views and the Readme file

--- src/lib/model/ViewNode.ts
+++ src/lib/model/ViewNode.ts
@@ -1,12 +1,12 @@
 export interface ViewNode {
-  _id: string;
+  _id?: string;
   modelNodeId: string;
   viewNodeId: string;
   name: string;
   type: string;
-  x: number;
-  y: number;
-  width: number;
-  height: number;
+  x?: number;
+  y?: number;
+  width?: number;
+  height?: number;
   parent?: string | null;
 }
